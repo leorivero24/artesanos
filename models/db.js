ca4de43b3a1bdc173003feccb1a535de30b9c20e
leorivero24 <leo.exerivero@gmail.com>
2025-06-17 11:54:14 -0300

    commit

--- models/db.js
+++ models/db.js
@@ -1,21 +1,21 @@
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',         // tu usuario de MySQL
-//   password: '',         // tu contraseña de MySQL
-//   database: 'artesanos' // nombre de la base de datos
-// });
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',         // tu usuario de MySQL
+  password: '',         // tu contraseña de MySQL
+  database: 'artesanos' // nombre de la base de datos
+});
 
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error al conectar con la base de datos:', err);
-//     return;
-//   }
-//   console.log('🟢 Conectado a la base de datos artesanos');
-// });
+db.connect((err) => {
+  if (err) {
+    console.error('Error al conectar con la base de datos:', err);
+    return;
+  }
+  console.log('🟢 Conectado a la base de datos artesanos');
+});
 
-// module.exports = db;
+module.exports = db;
 
 
 // const mysql = require('mysql2');
@@ -41,30 +41,30 @@
 // module.exports = db;
 
 
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-// Creamos un pool de conexiones
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// // Creamos un pool de conexiones
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('❌ Error al obtener conexión desde el pool:', err.message);
-  } else {
-    console.log('🟢 Pool conectado a la base de datos MySQL en Railway');
-    connection.release();
-  }
-});
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error('❌ Error al obtener conexión desde el pool:', err.message);
+//   } else {
+//     console.log('🟢 Pool conectado a la base de datos MySQL en Railway');
+//     connection.release();
+//   }
+// });
 
-module.exports = pool;
+// module.exports = pool;
 
 
 
